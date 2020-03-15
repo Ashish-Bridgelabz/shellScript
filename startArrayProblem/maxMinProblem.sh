569d54857127f5de7428fc34d215620bff29cb5b
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-15 15:34:57 +0530

    ADDED:Finded second maximum and minimum value

---
+++ startArrayProblem/maxMinProblem.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+echo "Generating ten random value" 
+for(( i=0 ; i < 10 ; i++ ))
+	do
+	array[i]=$((RANDOM%900+100))
+	done
+echo "${array[@]}"
+#FINDING SECOND MAXIMUM VALUE 
+max1=0
+max2=0
+for(( i =0 ; i < 10 ; i++ ))
+do
+	if(( ${array[i]} > $max1  ))
+	then
+		 max2=$max1
+		 max1=${array[i]}
+	fi
+done
+for(( i=0 ; i<10 ; i++))
+do
+	if((  ${array[i]} > $max2  && $max1 > ${array[i]} ))
+   then
+		max2=${array[i]}
+	fi
+done
+echo "Second maximum value is : $max2"
+
+#FINDING SECOND MINIMUM VALUE
+min1=1001
+min2=1001
+for(( i=0 ; i < 10 ; i++ ))
+do
+   if(( ${array[i]} < $min1  ))
+   then
+       min2=$min1
+       min1=${array[i]}
+   fi
+done
+for(( i=0 ; i<10 ; i++))
+do
+   if((  ${array[i]} < $min2  && $min1 < ${array[i]} ))
+   then
+      min2=${array[i]}
+   fi
+done
+echo "Second minimum value is :$min2"
+
